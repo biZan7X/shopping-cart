15402da265f9cffcfef93549024385103ba95a47
Biswanath Tewari <biswanath@geekyants.com>
2021-09-26 18:53:38 +0530

    feat - filter of products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import ProductCard from "./ProductCard";
 
+import { resetProducts, setProducts } from "../actions";
+
 import styles from "./styles.css";
 
 function Products({ products, filter }) {
-  useEffect(() => {
-    console.log(filter);
-  }, [filter]);
+  const { order, delivery, stock, rating } = filter;
+
+  const transformProducts = () => {
+    let filteredProducts = products;
+
+    filteredProducts.sort((a, b) =>
+      order === "lowToHigh" ? a.price - b.price : b.price - a.price
+    );
+
+    if (delivery)
+      filteredProducts = filteredProducts.filter((prod) => prod.fastDelivery);
+
+    if (stock)
+      filteredProducts = filteredProducts.filter((prod) => prod.inStock);
+
+    filteredProducts = filteredProducts.filter((prod) => prod.ratings > rating);
+
+    return filteredProducts;
+  };
+
   return (
     <div className="products">
-      {products.map((prod) => (
+      {transformProducts().map((prod) => (
         <ProductCard prod={prod} />
       ))}
     </div>
